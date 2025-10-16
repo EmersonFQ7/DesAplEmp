7df4c35a66b320919ac8a3b15fd0ac4a93f065b8
Emerson Flores <emerson.flores.q@tecsup.edu.pe>
2025-10-15 22:40:38 -0500

    Implementacion de ViewSets y CRUD

--- Laboratories/learnify_api_project/courses/views.py
+++ Laboratories/learnify_api_project/courses/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Instructor, Curso
+from .serializers import InstructorSerializer, CursoSerializer
 
-# Create your views here.
+class InstructorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite ver, crear, editar y eliminar Instructores.
+    """
+    queryset = Instructor.objects.all()
+    serializer_class = InstructorSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint para Cursos.
+    Permite búsqueda por nombre y nivel a través del parámetro 'search'.
+    Ej: /api/cursos/?search=Python
+    """
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'nivel']
